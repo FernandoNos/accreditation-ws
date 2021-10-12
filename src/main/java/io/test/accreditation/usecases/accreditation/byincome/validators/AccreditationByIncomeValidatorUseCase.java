b1dd819bd34a2127699b8b6316c1e016fb5db502
Fernando Nos <fernando461@gmail.com>
2021-10-11 23:55:35 -0300

    removing unused model

--- src/main/java/io/test/accreditation/usecases/accreditation/byincome/validators/AccreditationByIncomeValidatorUseCase.java
+++ src/main/java/io/test/accreditation/usecases/accreditation/byincome/validators/AccreditationByIncomeValidatorUseCase.java
@@ -1,6 +1,5 @@
 package io.test.accreditation.usecases.accreditation.byincome.validators;
 
-import io.test.accreditation.models.AccreditationValidationResult;
 import io.test.accreditation.models.UserAccreditationRequest;
 import reactor.core.publisher.Mono;
 
